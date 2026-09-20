cbdadcb955bb2e0f2f8f6eb0df1eac4b668504b5
jasmineolasiman <jasmineolasiman48@gmail.com>
2026-09-20 16:10:41 +0800

    i added different input for my header

---
+++ first-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Simple Bill Calculator</h1>
+      <p>Calculate your electric, water, food, and other bills</p>
+    </header>
+  );
+}
+
+export default Header;
